445733b22313eba854159201f4806adbbd09ce64
Anna Vorontsova <a.vorontsova@samsung.com>
2023-12-17 22:27:09 +0300

    Remove redundant functions from JS scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,75 +1,7 @@
-/*!
-    Title: Dev Portfolio Template
-    Version: 1.2.2
-    Last Change: 03/25/2020
-    Author: Ryan Fitzgerald
-    Repo: https://github.com/RyanFitzgerald/devportfolio-template
-    Issues: https://github.com/RyanFitzgerald/devportfolio-template/issues
-
-    Description: This file contains all the scripts associated with the single-page
-    portfolio website.
-*/
-
 (function($) {
-
-    // Show current year
-    $("#current-year").text(new Date().getFullYear());
 
     // Remove no-js class
     $('html').removeClass('no-js');
-
-    // Animate to section when nav is clicked
-    $('header a').click(function(e) {
-
-        // Treat as normal link if no-scroll class
-        if ($(this).hasClass('no-scroll')) return;
-
-        e.preventDefault();
-        var heading = $(this).attr('href');
-        var scrollDistance = $(heading).offset().top;
-
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
-
-        // Hide the menu once clicked if mobile
-        if ($('header').hasClass('active')) {
-            $('header, body').removeClass('active');
-        }
-    });
-
-    // Scroll to top
-    $('#to-top').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
-    });
-
-    // Scroll to first element
-    $('#main-lead-down span').click(function() {
-        var scrollDistance = $('#main-lead').next().offset().top;
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, 500);
-    });
-
-    // Scroll to second element
-    $('#main-guide-down span').click(function() {
-        var scrollDistance = $('#main-guide').next().offset().top;
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, 500);
-    });
-
-    // Open mobile menu
-    $('#mobile-menu-open').click(function() {
-        $('header, body').addClass('active');
-    });
-
-    // Close mobile menu
-    $('#mobile-menu-close').click(function() {
-        $('header, body').removeClass('active');
-    });
 
     $(".label").mousedown(function(e){
         $(this).addClass("active");
@@ -78,6 +10,5 @@
     }).mouseout(function(){
         $(this).removeClass("active");
     });
-
 
 })(jQuery);
